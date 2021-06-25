e51ec3f854ecf259f8ce817a770405bebf509e31
Jinseok Lee <paskal3314@gmail.com>
2021-06-25 00:06:37 -0600

    Hero component done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,35 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <section id="header">
+    <header>
       <Container>
-        <p>
-          Header
-        </p>
+        <div className="header-icons-container">
+          <a href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src="images/facebook-icon.png" /> 
+          </a>
+          <a href="https://www.google.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src="images/google-plus-icon.png" /> 
+          </a>
+          <a href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src="images/twitter-icon.png" /> 
+          </a>
+        </div>
+
+        <div className="header-hero-main">
+          <img src="images/JNJ-landingpage-hero-copy.png" />
+        </div>
+
+        <div className="header-hero-scroll">
+          <img src="images/JNJ-landingpage-hero-scroll-icon.png" />
+        </div>
       </Container>
-    </section>
+    </header>
   );
 }
 
