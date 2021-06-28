b41e42e191d4f29bc0f489324f0a59015060b4f8
Jinseok Lee <paskal3314@gmail.com>
2021-06-27 23:54:00 -0600

    Updated README, changed section to footer in footer comp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <section id="footer">
+    <footer id="footer">
       <Container>
         <p>Powered by Marketing4ecps Inc. 2017</p>
       </Container>
-    </section>
+    </footer>
   );
 };
 
